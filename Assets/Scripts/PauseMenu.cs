4cedffe2a8a96ecdd9db48c75c77e98a5e85f7fd
Leo <57363740+KenThumper@users.noreply.github.com>
2024-01-24 16:15:21 +0100

    Built Pause menu + fixed lighting
    Temporary assets lul

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+   public static bool isGamePaused = false;
+
+    public GameObject pauseMenu;
+
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+        isGamePaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        isGamePaused = false;
+    }
+
+    public void Home()
+   {
+        SceneManager.LoadScene("Start menu");
+        Time.timeScale = 1;
+        isGamePaused = false;
+   }
+
+   
+
+   public void Restart()
+   {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
+        isGamePaused = false;
+   }
+
+    
+}
