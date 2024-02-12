b98cc4ef4e735d160e8d137fc4c1a4db4797c472
Frikkz <106763788+Frikkz@users.noreply.github.com>
2024-02-12 14:41:24 +0100

    Fixed Explosions.

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -7,28 +7,6 @@ using UnityEngine;
 
 public class CubeScript : MonoBehaviour
 {
-    public GameObject Explosion;
-
-    public void OnCollisionEnter(Collision collision){
-        if (collision.gameObject.layer == this.gameObject.layer) {
-            //UnityEngine.Debug.Log("1");
-            //Instantiate(Explosion, transform.position, Quaternion.identity);
-            Destroy(this.gameObject);
-            
-        }
-        else if (collision.gameObject.layer == this.gameObject.layer) {
-            //Instantiate(Explosion, transform.position, Quaternion.identity);
-            //UnityEngine.Debug.Log("2");
-            Destroy(this.gameObject);
-            
-            
-        }
-        //Instantiate(Explosion, transform.position, Quaternion.identity);
-        //UnityEngine.Debug.Log("3");
-
-
-    }
-
     public int speed;
     private int delay = 30;
     // Start is called before the first frame update
--- Assets/Scripts/collision.cs
+++ Assets/Scripts/collision.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class collision : MonoBehaviour
+
 {
-    
+    public GameObject Explosion;
+
     public void OnCollisionEnter(Collision collision){
         if (collision.gameObject.layer == this.gameObject.layer) {
             Destroy(transform.parent.gameObject);
+            GameObject exp = Instantiate(Explosion, transform.position, Quaternion.identity);
+            Destroy(exp, 3);
         }
-        else if (collision.gameObject.layer == this.gameObject.layer) {
-            Destroy(transform.parent.gameObject);
-        }
-        
     }
     // Start is called before the first frame update
     void Start()
