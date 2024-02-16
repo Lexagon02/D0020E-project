892f52d2a01285e591c624871f17bfea283d554c
Lexagon02 <leo.r.bergstrom@gmail.com>
2024-02-16 15:09:13 +0100

    d

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,51 +4,57 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-
     public GameObject[] cubes;
     public Transform[] points;
-    public float beat; // beat of the song
-    private float timer;
+    private double beat; // beat of the song
+    private double timer;
+    private int i = 0;
 
-    private int[] color = {0,0,0,1,1,1,0,0,1,1,0,0,0,0,1,1,1,1}; // 0 for blue 1 for red
-    private int[] pos = {}; // 0, 1, 2, 3
-    private int[] rot = {}; // 0, 1, 2, 3
+    int[] color = new int[400];
+    int[] pos = new int[400];
 
-    //private arr[] arr = [[1,1,4,0], []]; possible array to spawn boxes
     // Start is called before the first frame update
     void Start()
     {
-        Difficulity();
-        // Create a temporary reference to the current scene.
-		
-
-		// Retrieve the name of this scene. 
-    }
-    private void Difficulity()
-    {
-        if (gameObject.tag == "Main Easy") 
+        //int[] color = {};
+        //int[] pos = {};
+        int diff = PlayerPrefs.GetInt("Difficulty");
+        if (diff == 1) 
 		{
-            Debug.Log("easy");
-		}
-		else if (gameObject.tag == "Main Medium")
+           
+            beat = 1.142857;
+            int[] color = {1,1,1,2,2,2,1,1,2,2,1,1,1,1,2,2,2,2};
+            int[] pos = {2,3,2,0,1,0,2,3,0,1,3,2,3,2,1,0,1,0}; // 0 (botRight), 1(TopRight), 2(botLeft), 3(topLeft)
+        }
+		else if (diff == 2)
 		{
-            Debug.Log("medium");
-		}
-        else if (gameObject.tag == "Main Hard")
+            beat = 1.142857;
+            int[] color = {0,0,0,1,1,1,0,0,1,1,0,0,0,0,1,1,1,1}; // 0 for blue 1 for red
+            int[] pos = {2,3,2,0,1,0,2,3,0,1,3,2,3,2,1,0,1,0}; // 0 (botRight), 1(TopRight), 2(botLeft), 3(topLeft)
+        }
+        else if (diff == 3)
 		{
-            Debug.Log("hard");
             
+            beat = 1.142857;
+            int[] color = {0,0,0,1,1,1,0,0,1,1,0,0,0,0,1,1,1,1}; // 0 for blue 1 for red
+            int[] pos = {2,3,2,0,1,0,2,3,0,1,3,2,3,2,1,0,1,0}; // 0 (botRight), 1(TopRight), 2(botLeft), 3(topLeft)
 		}
     }
+        
     // Update is called once per frame
     void Update()
     {
         
         if(timer>beat){
-            GameObject cube = Instantiate(cubes[Random.Range(0,2)], points[Random.Range(0,4)]); // Spawn cube at random spawn location
-            cube.transform.localPosition = Vector3.zero;
-            cube.transform.Rotate(transform.forward, 90 * Random.Range(0, 4));                  // Rotate around its axel
-            timer -= beat;
+            if(color[i] != 3){
+                GameObject cube = Instantiate(cubes[color[i]], points[pos[i]]); // Spawn cube at random spawn location
+                cube.transform.localPosition = Vector3.zero;
+                timer -= beat;
+                i++;
+            }
+            else{
+                i++;
+            }
         }
 
         timer += Time.deltaTime;
