cc13ee983a752ffaebc656b1ea503e9dd7b2be51
osaaxe-1 <oscar.axelstrom@hotmail.se>
2024-02-19 13:14:57 +0100

    Fixed explosions and collision

--- Assets/Scripts/collision.cs
+++ Assets/Scripts/collision.cs
@@ -1,13 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+//using System.Media;
 using UnityEngine;
 
 public class collision : MonoBehaviour
 {
+<<<<<<< Updated upstream
     
     public void OnCollisionEnter(Collision collision){
         if (collision.gameObject.layer == this.gameObject.layer) {
             Destroy(transform.parent.gameObject);
+=======
+    AudioManager audioManager;
+    public GameObject Explosion;
+    public CubeScript cube;
+    private Rigidbody rb;
+    private void Awake()
+    {
+        audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
+    }
+
+    public void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.layer == this.gameObject.layer) {
+            UnityEngine.Debug.Log(this.gameObject);
+            cube = transform.parent.GetComponent<CubeScript>();
+            cube.speed = 8;
+            rb=cube.gameObject.AddComponent<Rigidbody>();
+            transform.parent.gameObject.layer=9;
+            rb.velocity = new Vector3(3, -15, 0);
+            //rb.AddTorque(UnityEngine.Random.Range(0, 100), UnityEngine.Random.Range(0, 100), UnityEngine.Random.Range(0, 100));
+            Destroy(transform.parent.gameObject,3);
+            GameObject exp = Instantiate(Explosion, transform.position, Quaternion.identity);
+            Destroy(exp, 3);
+            GameManager.ScoreCount++;
+            GameManager.ScoreBonus++;
+            audioManager.PlaySFX(audioManager.BoxDestroyed);
+
+>>>>>>> Stashed changes
         }
         else if (collision.gameObject.layer == this.gameObject.layer) {
             Destroy(transform.parent.gameObject);
